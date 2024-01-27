77f3f5ed6c5b6c278b5c0b22ee0bc41468622879
fienestar <fienestar@gmail.com>
2024-01-28 00:47:47 +0900

    upload files

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Gentle Whip](https://spelunky.fyi/mods/m/gentle-whip/)
+
+The whip has no damage
+- all participants in the game must use this mode.
---
+++ main.lua
@@ -0,0 +1,9 @@
+meta = {
+	name = "Gentle Whip",
+	version = "0.1",
+	description = "The whip has no damage",
+	author = "fienestar",
+	online_safe = true,
+}
+
+get_type(ENT_TYPE.ITEM_WHIP).damage = 0
